d9cc23c7638691ec9debe7c07c6ba2cade202f32
Aksh Bansal <akshmark@gmail.com>
2021-05-02 19:03:25 +0530

    C++ template added

---
+++ cscript.sh
@@ -0,0 +1,1 @@
+git clone https://github.com/yatharthagoenka/CPP-Template.git
--- index.js
+++ index.js
@@ -1,7 +1,32 @@
 #!/usr/bin/env node
 
 const shell = require("shelljs");
+const inquirer = require("inquirer");
 
-console.log("Cloning the template...")
-shell.exec(`${process.cwd()}/script.sh`);
-console.log("Done!");
+inquirer
+  .prompt([
+      {type: "list", message: "Select the language: ", name: "lang", choices: [
+        "Java",
+        "C++"
+      ]}
+
+  ])
+  .then(answers => {
+    console.log("Cloning the template...")
+    if(answers.lang==="Java"){
+        shell.exec(`${process.cwd()}/jscript.sh`);
+        console.log("Done!");
+    }
+    else if(answers.lang==="C++"){
+        shell.exec(`${process.cwd()}/cscript.sh`);
+        console.log("Done!");
+    }
+    else{
+      console.log("ERROR!!\n Make sure you have git and npm installed.");
+    }
+
+  })
+  .catch(error => {
+      console.log("ERROR!!\n Make sure you have git and npm installed.");
+  });
+
